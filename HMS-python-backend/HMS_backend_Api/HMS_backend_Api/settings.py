33959511aef5559a6a8d10a9b28acd1a41c8be1d
samuelsenerwa <samuelsenerwa@gmail.com>
2023-09-12 19:48:55 +0300

    Configured CORS

--- HMS-python-backend/HMS_backend_Api/HMS_backend_Api/settings.py
+++ HMS-python-backend/HMS_backend_Api/HMS_backend_Api/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # CORS
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +54,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
 
 ROOT_URLCONF = 'HMS_backend_Api.urls'
 
