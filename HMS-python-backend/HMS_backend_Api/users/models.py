29fe35e8e6e68896a927b6fd74b2126316679414
samuelsenerwa <samuelsenerwa@gmail.com>
2023-09-12 19:57:46 +0300

    created models

--- HMS-python-backend/HMS_backend_Api/users/models.py
+++ HMS-python-backend/HMS_backend_Api/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User (models.Model):
+    firstname = models.CharField(max_length=70, blank=False, default='')
+    lastname =  models.CharField(max_length=70, blank=False, default='')
+    email = models.CharField(max_length=70, blank=False, default='')
+    phone_number = models.CharField(max_length=20, blank=False, default='')
+    
